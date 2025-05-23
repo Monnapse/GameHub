72344cc7124cd7449574b9791509f5508d99f594
Monnapse <masonshuber@gmail.com>
2025-05-23 16:57:21 -0600

    save

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ router.get('/', (req, res) => {
   res.send('Welcome to the home page!');
 });
 
+router.get('/users', require('./users'));
+router.get('/games', require('./games'));
+
 module.exports = router;
